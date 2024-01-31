a212e57083a212b52e4ccf068133851d0626042c
majid <smkh1985@gmail.com>
2024-01-31 10:48:14 -0800

    modified:   product/models.py

--- product/models.py
+++ product/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
-# Create your models here.
+class Product(models.Model):
+    WATER_TYPES = [
+        ('SS', 'Salt Spring'),
+        ('O2', 'O2 Water'),
+        ('MDS', 'Regular Water'),
+        ('OZ', 'Ozone Water'),
+    ]
+
+    SIZE_CHOICES = [
+        (3, 'Large (5 Gallon)'),
+        (5, 'Small (3 Gallon)'),
+    ]
+
+    water_type = models.CharField("Water Type" , max_length = 3 , choices = WATER_TYPES)
+    bottle_size = models.IntegerField("Bottle Size", choices=SIZE_CHOICES)
+    price = models.DecimalField(max_digits = 5 , decimal_places = 2)
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(fields=['water_type', 'bottle_size'], name='water_type_size')
+        ]
+
+
+
+
+    
